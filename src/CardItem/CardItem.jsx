1f46950a28a1dff6f25a204a1ee61ad3bdcd9159
irynaMatlayeva <iryna.matlayeva@gmail.com>
2021-09-27 23:08:28 +0300

    second solution with function for responsive text

--- src/CardItem/CardItem.jsx
+++ src/CardItem/CardItem.jsx
@@ -1,27 +1,29 @@
 import React from 'react';
 import { useViewport } from '../store/viewportContext';
+import { cards } from '../mock';
 import './CardItem.scss';
 
-const CardItem = ({card}) => {
-  const {id, img, altText, title, oldPrice, currentPrice} = card
+const CardItem = () => {
   const {width} = useViewport();
   const breakpoint = 769;
 
   return (
-    <li className={ `card-item card-item--${ id }` } key={ id }>
-      <div className='card-item__img-container'>
-        <img className='card-item__img' src={ img } alt={ altText }/>
-      </div>
+    cards.map(card =>
+      <div className={ `card-item card-item--${ card.id }` } key={ card.id }>
+        <div className='card-item__img-container'>
+          <img className='card-item__img' src={ card.img } alt={ card.altText }/>
+        </div>
 
-      <div className='card-item__wrap'>
-        <h2 className='card-item__title'>
-          <a href='#void' className='card-item__link'>{ title }</a>
-        </h2>
-        <small className='card-item__id'>SKU: { id } </small>
-        <p className='card-item__old-price'>{ width < breakpoint ? `${ Math.floor(oldPrice) } $` : `${ oldPrice } $` }</p>
-        <p className='card-item__current-price'>{ width < breakpoint ? `${ Math.round(currentPrice) } $` : `${ currentPrice } $` }</p>
+        <div className='card-item__wrap'>
+          <h2 className='card-item__title'>
+            <a href='#void' className='card-item__link'>{ card.title }</a>
+          </h2>
+          <small className='card-item__id'>SKU: { card.id } </small>
+          <span className='card-item__old-price'>{ width < breakpoint ? `${ Math.floor(card.oldPrice) } $` : `${ card.oldPrice } $` }</span>
+          <span className='card-item__current-price'>{ width < breakpoint ? `${ Math.round(card.currentPrice) } $` : `${ card.currentPrice } $` }</span>
+        </div>
       </div>
-    </li>
+    )
   )
 }
 
